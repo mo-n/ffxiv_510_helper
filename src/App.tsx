678c1311f7c548e297f8b16fd32ed7959224ff67
mo_n <mo_n.tt@outlook.com>
2021-03-25 18:54:14 +0800

    add  view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { observer } from "mobx-react-lite";
+
+import { Store } from "store";
+// import Timer from "components/Timer";
+import Armorys from "components/Armorys";
+import Arms from "components/Arms";
+
+import "./App.css";
+import "assets/number.css";
+
+const store = new Store()
 
 function App() {
+  const EquipmentView = observer(Armorys);
+  const ArmsView = observer(Arms);
   return (
     <div className="App">
-      <header>
-      </header>
+      <main className="grid">
+        <div>
+          {/* <Timer /> */}
+        </div>
+        <div>
+          <EquipmentView store={store}></EquipmentView>
+        </div>
+        <div>
+          <ArmsView store={store} />
+        </div>
+        {/* 点数 */}
+        <div>
+
+        </div>
+        {/* 采集材料 */}
+        <div>
+
+        </div>
+        {/* 半成品 */}
+        <div>
+
+        </div>
+        {/* 列表 */}
+        <div>
+
+        </div>
+      </main>
     </div>
   );
 }
