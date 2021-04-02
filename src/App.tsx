fde35fc210fa48b49933b24e7005cd493a197684
mo_n <mo_n.tt@outlook.com>
2021-04-02 18:03:50 +0800

    修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -19,27 +19,30 @@ function App() {
   const [store] = useState(() => new Store())
 
   return (
-    <div className="App w-screen h-screen pt-8 overflow-hidden">
-      <main className=" m-auto">
-        <div className="grid auto-rows-max grid-cols-3 justify-items-center grid-flow-col gap-3 auto-cols-fr">
-          <div className="w-full rounded shadow bg-white">
+    <div className="App w-screen md:h-screen pt-8 overflow-hidden">
+      <main className="m-auto sm:container md:container">
+        <div className="grid auto-rows-max grid-cols-1 md:grid-cols-4 grid-flow-col justify-items-center gap-3">
+          <div className="w-96 rounded shadow row-span-3 bg-white">
+
+          </div>
+          <div className="w-80 rounded shadow bg-white">
             <Timer />
           </div>
-          <div className="w-full rounded shadow bg-white">
+          <div className="w-80 rounded shadow bg-white">
             <Armorys store={store} />
           </div>
-          <div className="w-full rounded shadow bg-white">
+          <div className="w-80 rounded shadow bg-white">
             <Arms store={store} />
           </div>
           {/* 半成品 */}
-          <div className="w-full rounded shadow row-span-2 bg-white">
+          <div className="w-80 rounded shadow row-span-2 bg-white">
             <Crafts list={store.crafts} />
           </div>
-          <div className="w-full rounded shadow row-span-1 bg-white">
+          <div className="w-80 rounded shadow row-span-1 bg-white">
             <Vendors list={store.rawMaterial} />
           </div>
           {/* 素材 */}
-          <div className="w-full rounded shadow row-span-3 bg-white">
+          <div className="w-80 rounded shadow row-span-3 bg-white">
             <RawMaterial list={store.rawMaterial}/>
           </div>
         </div>
